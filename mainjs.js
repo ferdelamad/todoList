c54ff7370eac60d8d2315cc0fc364dbb18eb1b80
Fernando <ferdelamad@gmail.com>
2018-04-20 01:16:21 -0700

    Update to the Todo List

--- mainjs.js
+++ mainjs.js
@@ -5,10 +5,15 @@ var todoList = {
   displayTodos: function() {
     var todo = this.todos
     if (!todo.length) {
-      console.log("You don't have todos!")
+      console.log("You Todos list is empty!")
     } else {
-        for (var i = 0; i < todo.length; i++) {
-          console.log('My todo:', todo[i].todoText)
+        console.log('My Todos:');
+        for (var i = 0; i < todo.length; i++)   {
+          if (todo[i].completed === true) {
+            console.log('(x)', todo[i].todoText);
+          } else {
+            console.log('( )', todo[i].todoText);
+          }
         }
       }
   },
@@ -31,5 +36,45 @@ var todoList = {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
     this.displayTodos();
+  },
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    //Get number of completed todos
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    //Case 1: If everything's true.
+    if (totalTodos === completedTodos) {
+      //make everything false.
+      this.todos.forEach(function(todo) {
+        todo.completed = false;
+      });
+      //Case 2: Otherwise make everything true.
+    } else {
+        this.todos.forEach(function(todo) {
+          todo.completed = true;
+        });
+    }
+    this.displayTodos();
   }
+
 };
+
+//We want to access the Display Todos button
+var displayTodosButton = document.getElementById('displayTodosButton')
+  //We want to run the displayTodos Method when someone clicks the display todos button
+  //Add an event
+displayTodosButton.addEventListener('click', function() {
+  todoList.displayTodos();
+});
+
+//We want to access the Toggle All button
+var toggleAllButton = document.getElementById('ToggleAllButton');
+  //We want to run the toggleAll Method when someone clicks the toggle all button
+  //Ad an event
+toggleAllButton.addEventListener('click', function() {
+  todoList.toggleAll();
+});
